e17f9fbf96f32fbab9c32b4661f1510b3a7cd606
ILnur Saifutdinov <ilnursoft@gmail.com>
2021-03-17 17:50:59 +0300

    поменял концепцию настроек. Вводится класс User который хранит данные конкретного пользователя

--- sqlitelib/sqliteutils.py
+++ sqlitelib/sqliteutils.py
@@ -9,54 +9,155 @@
 
 import os
 import sqlite3
+from enum import Enum
 
 
-def createnewdb(namedb='settings.db', force=False):
-    """
-        создание БД настроек бота
-            namedb - название БД
-            force  - если True, то даже если БД существует, оно перезапишет его
-        возращает True, если операция создания успешно.
-    """
-    try:
-        if os.path.exists(namedb) and force:
-            # print('Файл существует')
-            os.remove(namedb)
+# доступные роли пользователя
+class Role(Enum):
+    admin = 1
+    user = 2
 
-        conn = sqlite3.connect(namedb)
-        cursor = conn.cursor()
 
+# типы результата работы
+class TypeResult(Enum):
+    video = 1
+    sound = 2
+
+
+# данные конкретного пользователя
+class User:
+
+    def __init__(self, id=-1):
+        self._id = id
+        self._name = ''
+        self._active = False
+        self._role = Role.user
+        self._typeresult = TypeResult.sound
+        self._qualityresult = None
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, my_id):
+        self._id = my_id
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
+
+    @property
+    def active(self):
+        return self._active
+
+    @active.setter
+    def active(self, flag):
+        if flag == 0:
+            self._active = False
+        else:
+            self._active = True
+
+    @property
+    def role(self):
+        return self._role
+
+    @role.setter
+    def role(self, new_role):
+        # проверить соответствует ли new_role классу Role
+        self._role = new_role
+
+    @property
+    def typeresult(self):
+        return self._typeresult
+
+    @typeresult.setter
+    def typeresult(self, new_type):
+        # проверить соответствует ли new_type классу TypeResult
+        self._typeresult = new_type
+
+    @property
+    def qualityresult(self):
+        return self._qualityresult
+
+    @qualityresult.setter
+    def qualityresult(self, quality):
+        self._qualityresult = quality
+
+    def __str__(self):
+        return f"User -> id: {self.id}\n\tname: {self.name}\n\tactive: {self.active}\n\t" \
+               f"role: {self._role}\n\ttyperesult: {self._typeresult}\n\tqualityresult: {self._qualityresult} "
+
+
+class SettingUser:
+
+    def __init__(self, namedb='settings.db', force=False):
         """
-            Создание таблицы USER - информация о пользователях
-            поля: 
-                id - id пользователя из телеграмма
-                name - имя пользователя
+            нициализация БД настроек бота
+                namedb - название БД
+                force  - если True, то даже если БД существует, оно перезапишет его
         """
-        cursor.execute("""CREATE TABLE user 
-                          (id INTEGER, name text)
-                       """)
+        self.db = namedb  # имя БД настроек бота
+        # проверим есть ли вообще БД
+        self.__createnewdb(force)
 
+    def __createnewdb(self, force=False):
         """
-            Создание таблицы settings  - информация о настройках бота
-            поля:
-                id - id пользователя из телеграмма (связана с полем id таблицы USER 
-                active - если 0, пользователь неактивный, иначе пользователь активный
-                role - роль пользователя: admin - администратор бота, user - обычный пользователь бота
-                typeresult - результат работы бота: видео со звуком или только звук
-                qualityresult - качество видео или звука
+            создание БД настроек бота
+            возращает True, если операция создания успешно.
         """
-        cursor.execute("""CREATE TABLE settings 
-                          (id INTEGER, active INTEGER, role text, typeresult text, qualityresult text)
+        try:
+            if os.path.exists(self.db):
+                if force:
+                    # print('Файл существует')
+                    os.remove(self.db)
+            else:
+                return True
+
+            conn = sqlite3.connect(self.db)
+            cursor = conn.cursor()
+
+            """
+                Создание таблицы USER - информация о пользователях
+                поля: 
+                    id - id пользователя из телеграмма
+                    name - имя пользователя
+                    active - если 0, пользователь неактивный, иначе пользователь активный
+            """
+            cursor.execute("""CREATE TABLE user 
+                      (id INTEGER, name text, active INTEGER)
                    """)
-    except sqlite3.Error as error:
-        print("Ошибка при подключении к sqlite", error)
-        return False
-    finally:
-        if (conn):
-            conn.close()
-            # print("Соединение с SQLite закрыто")
-    return True
+
+            """
+                Создание таблицы settings  - информация о настройках бота
+                поля:
+                    id - id пользователя из телеграмма (связана с полем id таблицы USER 
+                    role - роль пользователя: admin - администратор бота, user - обычный пользователь бота
+                    typeresult - результат работы бота: видео со звуком или только звук
+                    qualityresult - качество видео или звука
+            """
+            cursor.execute("""CREATE TABLE settings 
+                      (id INTEGER, role text, typeresult text, qualityresult text)
+               """)
+        except sqlite3.Error as error:
+            print("Ошибка при подключении к sqlite", error)
+            return False
+        finally:
+            if (conn):
+                conn.close()
+                # print("Соединение с SQLite закрыто")
+        return True
 
 
 if __name__ == '__main__':
-    createnewdb('settings_test.db', True)
+    new_user = User()
+    print(new_user)
+    new_user.id = 458999
+    new_user.name = 'TestUser'
+    new_user.active = False
+    print(new_user)
+    # sUser = SettingUser('settings_test.db', False)
