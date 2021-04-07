33b44cbab7dbde09f29f59e410ac758aaba8767e
ILnur Saifutdinov <ilnursoft@gmail.com>
2021-04-07 11:17:07 +0300

    добавлены типы функций

--- i_utils.py
+++ i_utils.py
@@ -3,23 +3,24 @@
  Author: Ilnur Sayfutdinov
  E-mail: ilnursoft@gmail.com
 """
+from typing import Tuple, Optional
 
 import requests
 import asyncio
+from codecs import escape_decode  # type: ignore
 
 
 # преобразование строки вида "SAMSUNG a50 64\\xd0\\xb3\\xd0\\xb1" в строку c кодировкой encoding
-def string_escape(s, encoding="utf-8"):
-    import codecs
+def string_escape(s: str, encoding: str = "utf-8") -> str:
     byte_s = bytes(s, encoding)
-    res = codecs.escape_decode(byte_s)[0]
+    res = escape_decode(byte_s)[0]
     res = res.decode(encoding)
     return res
 
 
 # сохранить в файл html из urls
 # True - если смогли получить данные с запроса и сохранить в файл
-def savefile_from_url(urls=None, filename="test.html"):
+def savefile_from_url(urls: Optional[str] = None, filename: str = "test.html") -> bool:
     if urls is None:
         return False
     session = requests.Session()
@@ -38,20 +39,18 @@ def savefile_from_url(urls=None, filename="test.html"):
 
 
 # запуск команды cmd asyncio
-async def run_cmd(cmd):
+async def run_cmd(cmd: str) -> Tuple[bytes, bytes, Optional[int]]:
     proc = await asyncio.create_subprocess_shell(
         cmd,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE)
-    
+
     stdout, stderr = await proc.communicate()
 
-    print(f'[{cmd!r} exited with {proc.returncode}]')    
+    print(f'[{cmd!r} exited with {proc.returncode}]')
     if stdout:
-        print(f'[stdout]\n{stdout.decode()}')          
-    # if stderr:
+        print(f'[stdout]\n{stdout.decode()}')
+        # if stderr:
     #     print(f'[stderr]\n{stderr.decode()}')
 
     return stdout, stderr, proc.returncode
-
-    
